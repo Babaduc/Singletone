ebaee0a9365ec34481bc486ee384c339164d0dfd
Babaduc <47734243+Babaduc@users.noreply.github.com>
2019-03-09 00:12:46 +0200

    Add files via upload

---
+++ Singletone/SingleTone/SingleTone/Singletone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingleTone
+{
+    class Singletone
+    {
+        private static Singletone Instance;
+        public static Singletone GetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = new Singletone();
+        }
+        return Instance;
+    }
+        private Singletone()
+        {
+            Console.WriteLine("Nice to meet you!");
+        }
+    }
+}
